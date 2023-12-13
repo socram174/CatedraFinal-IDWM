490ca9a2a12f74648ecf37070dfd41a683ad416a
Marcos Silva <marcos.silva@alumnos.ucn.cl>
2023-12-13 16:01:25 -0300

    eliminar listo

--- mobile/App.js
+++ mobile/App.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,  View, ScrollView } from 'react-native';
+import { StyleSheet, View, ScrollView, SafeAreaView } from 'react-native';
 import { AppRegistry } from 'react-native';
-import { PaperProvider,Avatar, Button, Card, Text } from 'react-native-paper';
+import { PaperProvider, Avatar, Button, Card, Text, Modal, Portal, } from 'react-native-paper';
 import { name as appName } from './app.json';
 
 
@@ -10,52 +10,120 @@ const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
 
 export default function App() {
+
+  const base_url = "https://vr4s1c1r-3000.brs.devtunnels.ms";
   const [posts, setPosts] = useState(null);
+  const [visible, setVisible] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [comments, setComments] = useState(null);
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = { backgroundColor: 'white', padding: 20 };
 
   const getPosts = async () => {
-try{
-  const response = await fetch('https://vr4s1c1r-3000.brs.devtunnels.ms/posts');
-  const data = await response.json();
-  setPosts(data);
-  console.log(data);
-}catch(e){
-  console.log(e);
-}
+    try {
+      const response = await fetch(`${base_url}/posts`);
+      const data = await response.json();
+      setPosts(data);
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const getComments = async () => {
+    try {
+      const response = await fetch(`${base_url}/comments`);
+      const data = await response.json();
+      setComments(data);
+      //console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
 
-  useEffect(() =>{
+  const deletePost = async (id) => {
+    comments.map(async (comment) => {
+      if(comment.postId === selectedPost){
+        const res = await fetch(`${base_url}/comments/${comment.id}`,{
+          method:"DELETE"
+        })
+      }
+    })
+    const res = await fetch(`${base_url}/posts/${id}`, {
+      method: "DELETE"
+    });
     getPosts();
-  },[]);
+  }
+
+  useEffect(() => {
+    getPosts();
+    getComments();
+  }, []);
 
 
-  if(!posts) return (<View style={{height:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}><Text>Cargando...</Text></View>)
+  if (!posts) return (<View style={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}><Text>Cargando...</Text></View>)
 
 
   return (
 
-        <PaperProvider>
-          <ScrollView>
-            <View >
-            {posts.map((post) => {
+    <PaperProvider>
+      <ScrollView>
+        <View style={{ padding: 10, gap: 15 }}>
+          {posts.map((post) => {
             return (
               <Card>
-              <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
-              <Card.Content>
-                <Text variant="titleLarge">{post.title}</Text>
-                <Text variant="bodyMedium">{post.author}</Text>
-              </Card.Content>
-              <Card.Cover source={{ uri: post.image }} />
-              <Card.Actions>
-                <Button>Cancel</Button>
-                <Button>Ok</Button>
-              </Card.Actions>
-            </Card>
+                <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+                <Card.Content>
+                  <Text variant="titleLarge">{post.title}</Text>
+                  <Text variant="bodyMedium">{post.author}</Text>
+                </Card.Content>
+                <Card.Cover source={{ uri: post.image }} />
+                <View>
+
+                  {comments && (
+                    comments.map((comment) => {
+                      if (comment.postId == post.id) {
+                        return (<>
+                          <View style={{padding:10}}>
+                          <Text>{comment.body}</Text>
+                          </View>
+                        </>)
+                      }
+                    })
+                  )}
+
+
+                </View>
+                <Card.Actions>
+                  <Button onPress={() => {
+                    setSelectedPost(post.id);
+                    showModal();
+
+                  }}>Eliminar</Button>
+                  <Button>Ok</Button>
+                </Card.Actions>
+              </Card>
             )
           })}
-            </View>
-          </ScrollView>
-      </PaperProvider>
+        </View>
+        <Portal>
+          <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+            <Text>¿Seguro que desea borrar el post?</Text>
+            <Button onPress={async () => {
+              deletePost(selectedPost);
+              //setSelectedPost(null);
+              hideModal();
+
+            }}>Si</Button>
+            <Button onPress={hideModal}>Cancelar</Button>
+          </Modal>
+        </Portal>
+      </ScrollView>
+
+    </PaperProvider>
   );
 }
 
