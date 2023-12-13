7a4c6619e11fe674d9a30f45c366a59019a32536
Marcos Silva <marcos.silva@alumnos.ucn.cl>
2023-12-13 15:17:05 -0300

    RU1516

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,61 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  View, ScrollView } from 'react-native';
+import { AppRegistry } from 'react-native';
+import { PaperProvider,Avatar, Button, Card, Text } from 'react-native-paper';
+import { name as appName } from './app.json';
+
+
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+
 
 export default function App() {
+  const [posts, setPosts] = useState(null);
+
+  const getPosts = async () => {
+try{
+  const response = await fetch('https://vr4s1c1r-3000.brs.devtunnels.ms/posts');
+  const data = await response.json();
+  setPosts(data);
+  console.log(data);
+}catch(e){
+  console.log(e);
+}
+  }
+
+
+  useEffect(() =>{
+    getPosts();
+  },[]);
+
+
+  if(!posts) return (<View style={{height:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}><Text>Cargando...</Text></View>)
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+        <PaperProvider>
+          <ScrollView>
+            <View >
+            {posts.map((post) => {
+            return (
+              <Card>
+              <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+              <Card.Content>
+                <Text variant="titleLarge">{post.title}</Text>
+                <Text variant="bodyMedium">{post.author}</Text>
+              </Card.Content>
+              <Card.Cover source={{ uri: post.image }} />
+              <Card.Actions>
+                <Button>Cancel</Button>
+                <Button>Ok</Button>
+              </Card.Actions>
+            </Card>
+            )
+          })}
+            </View>
+          </ScrollView>
+      </PaperProvider>
   );
 }
 
@@ -18,3 +67,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+AppRegistry.registerComponent(appName, () => App);
